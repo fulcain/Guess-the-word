7023478256f000cae45e4c143bef8c869f77bfd6
Majid Kargar <majidkargar01@gmail.com>
2023-09-21 11:13:38 +0430

    feat(setWord): Add do/while condition to avoid recursive call

--- src/script.js
+++ src/script.js
@@ -1,81 +1,79 @@
-//  Import getRandomWord helper function that fetches data from an API
+/**  Import getRandomWord helper function that fetches data from an API */
 import getRandomWord from "./utils/getRandomWord.js";
 
-//  Import setWrongCharacters helper function to pass the arrayOfWrong and the pressed key to the function
+/**  Import setWrongCharacters helper function to pass the arrayOfWrong and the pressed key to the function */
 import setWrongCharacters from "./utils/setWrongCharacters.js";
 
-// Import Line component to create a line for each character of the word
+/** Import Line component to create a line for each character of the word */
 import Line from "./components/line.js";
 
-// Maximum length required for the word
+/** Maximum length required for the word */
 const MAXIMUM_LENGTH_OF_WORD = 7;
 
-// Array to store the characters of the word
-let arrayOfCharacters = [];
-
-// Array to store wrong characters
+/** Array to store wrong characters */
 const arrayOfWrongCharacters = [];
 
-// Asynchronous function to set the word
+/** Array of characters to store each character of the word */
+let arrayOfCharacters = [];
+
+/** Asynchronous function to set the word */
 const setWord = async () => {
     try {
-        // Fetching a random word and extracting the 'word' property from the resolved value
-        const { word } = await getRandomWord().catch((err) =>
-            console.error(err)
-        );
+        let word;
 
-        // Checking if the word length is less than the maximum length required
-        if (word.length < MAXIMUM_LENGTH_OF_WORD) {
-            // Selecting the HTML element with the id "word"
-            const wordEl = document.querySelector("#word");
+        do {
+            /** Fetching a random word and extracting the 'word' property from the resolved value */
+            const { word: fetchedWord } = await getRandomWord().catch((err) =>
+                console.error(err)
+            );
 
-            // Clearing the content of the word element
-            wordEl.textContent = "";
+            word = fetchedWord;
+        } while (word.length >= MAXIMUM_LENGTH_OF_WORD);
 
-            // Storing the characters of the word in the arrayOfCharacters array
-            arrayOfCharacters = [...word];
+        /** Selecting the HTML element with the id "word" */
+        const wordEl = document.querySelector("#word");
 
-            // Looping through each character in the arrayOfCharacters array
-            arrayOfCharacters.forEach((char) => {
-                // Adding a keydown event listener to the window object
-                window.addEventListener("keydown", (e) => {
-                    // Checking if the pressed key is included in the arrayOfCharacters
-                    if (arrayOfCharacters.includes(e.key)) {
-                        // Selecting all DOM elements with the class name "line-value"
-                        const allLineValues =
-                            document.querySelectorAll(".line-value");
-                        // Looping through the arrayOfCharacters for applying the show classList
-                        for (let i = 0; i < arrayOfCharacters.length; i++) {
-                            // Checking if the current element in the arrayOfCharacters at index i is equal to the pressed key
-                            if (arrayOfCharacters[i] === e.key) {
-                                // Adding the "show" class to the corresponding element at index i in allLineValues
-                                allLineValues[i].classList.add("show");
-                            }
-                        }
+        /** Clearing the content of the word element */
+        wordEl.textContent = "";
+
+        /** Storing the characters of the word in the arrayOfCharacters array */
+        arrayOfCharacters = [...word];
+
+        /** Creating an array of Line components */
+        const lines = arrayOfCharacters.map((char) => Line(char));
+
+        /** Appending the Line components to the wordEl */
+        wordEl.append(...lines);
+
+        /** Adding a keydown event listener to the window object */
+        window.addEventListener("keydown", (e) => {
+            /** Checking if the pressed key is included in the arrayOfCharacters */
+            if (arrayOfCharacters.includes(e.key)) {
+                /** Selecting all DOM elements with the class name "line-value" */
+                const allLineValues = document.querySelectorAll(".line-value");
+
+                /** Looping through the arrayOfCharacters for applying the show classList */
+                for (let i = 0; i < arrayOfCharacters.length; i++) {
+                    /** Checking if the current element in the arrayOfCharacters at index i is equal to the pressed key */
+                    if (arrayOfCharacters[i] === e.key) {
+                        /** Adding the "show" class to the corresponding element at index i in allLineValues */
+                        allLineValues[i].classList.add("show");
                     }
-                    // If the pressed key is NOT included in the arrayOfCharacters this code is executed
-                    else {
-                        // Calling the setWrongCharacters function with the arrayOfWrongCharacters array and the pressed key
-                        setWrongCharacters(arrayOfWrongCharacters, e);
-                    }
-                });
-                // Appending a Line component with the current character to the wordEl
-                wordEl.append(Line(char));
-            });
-        }
-        // If the word length is greater than or equal to the minimum length, recursively call the setWord function
-        else {
-            setWord();
-        }
+                }
+            } else {
+                /** Calling the setWrongCharacters function with the arrayOfWrongCharacters array and the pressed key */
+                setWrongCharacters(arrayOfWrongCharacters, e);
+            }
+        });
     } catch (error) {
         console.error(error);
     }
 };
 
-// Event listener to initialize the game when the DOM content is loaded
+/** Event listener to initialize the game when the DOM content is loaded */
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    // Calling the setWord function to start the game
+    /** Calling the setWord function to start the game */
     setWord();
 }
