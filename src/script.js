bdbc63d6e564901b931fbd5a7ad37fe892866c94
Majid Kargar <majidkargar01@gmail.com>
2023-09-24 19:50:50 +0330

    Refactor: Refactor CSS and improve game functionality, add hint button
    Description:
    
    This commit refactors the CSS styles by adding styles for the new hint button and defines its behavior when clicked.
    
    It also improves the game functionality by adding the ability to give hints to the player.
    
    The hint button is disabled after being clicked and reveals a random character from the word that the player hasn't guessed yet. The commit also includes changes to HTML and JavaScript files related to the addition of the hint button and the logic behind handling hints and player input.
    
    Changelog:
    
    [index.html] [Added]: Hint button
    
    [script.js] [Refactored]: Refactor the file into multiple functions so that it makes the code more organized and easier to understand.
    
    [script.js] [Added]: Hint button functionallity

--- src/script.js
+++ src/script.js
@@ -4,25 +4,20 @@ import checkCorrectAnswer from "./utils/checkCorrectAnswer.js";
 import Line from "./components/line.js";
 import checkWrongAnswers from "./utils/checkWrongAnswers.js";
 
-/** Maximum length required for the word */
+const arrayOfGuessedCharacters = [];
+let arrayOfPossibleHints = [];
 const MAXIMUM_LENGTH_OF_WORD = 7;
-
-/** Array to store wrong characters */
 let arrayOfWrongCharacters = [];
-
-/** Array of characters to store each character of the word */
 let arrayOfCharacters = [];
+const hintButton = document.querySelector("#hint-button");
+let hintButtonClicked = false;
 
-/** Asynchronous function to set the word */
 const setWord = async () => {
     try {
-        /**
-         * @type {Object} - An object that is fetched from getRandomWord() function
-         */
+        hintButton.disabled = true;
         let word;
 
         do {
-            /** Fetching a random word and extracting the 'word' property from the resolved value */
             const { word: fetchedWord } = await getRandomWord().catch((err) =>
                 console.error(err)
             );
@@ -30,54 +25,80 @@ const setWord = async () => {
             word = fetchedWord;
         } while (word.length >= MAXIMUM_LENGTH_OF_WORD);
 
-        /** Selecting the HTML element with the id "word" */
         const wordEl = document.querySelector("#word");
 
-        /** Clearing the content of the word element */
         wordEl.textContent = "";
 
-        /** Storing the characters of the word in the arrayOfCharacters array */
         arrayOfCharacters = [...word];
 
-        /** Creating an array of Line components */
         const lines = arrayOfCharacters.map((char) => Line(char.toLowerCase()));
 
-        /** Appending the Line components to the wordEl */
         wordEl.append(...lines);
 
-        /** Adding a keydown event listener to the window object */
-        window.addEventListener("keydown", (e) => {
-            /** Checking if the pressed key is included in the arrayOfCharacters */
-            if (arrayOfCharacters.includes(e.key)) {
-                /** Selecting all DOM elements with the class name "line-value" */
-                const allLineValues = document.querySelectorAll(".line-value");
-
-                /** Looping through the arrayOfCharacters for applying the show classList */
-                for (let i = 0; i < arrayOfCharacters.length; i++) {
-                    /** Checking if the current element in the arrayOfCharacters at index i is equal to the pressed key */
-                    if (arrayOfCharacters[i] === e.key) {
-                        /** Add to the correctCharacterAnswers if the character is in the list */
-                        checkCorrectAnswer(arrayOfCharacters,e.key);
-
-                        /** Adding the "show" class to the corresponding element at index i in allLineValues */
-                        allLineValues[i].classList.add("show");
-                    }
-                }
-            } else {
-                /** Calling the setWrongCharacters function with the arrayOfWrongCharacters array and the pressed key */
-                setWrongCharacters(arrayOfWrongCharacters, e);
-                checkWrongAnswers(arrayOfWrongCharacters)
-            }
-        });
+        window.addEventListener("keydown", handleKeyDown);
     } catch (error) {
         console.error(error);
     }
 };
 
-/** Event listener to initialize the game when the DOM content is loaded */
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    /** Calling the setWord function to start the game */
     setWord();
 }
+
+hintButton.addEventListener("click", handleHintButtonClick);
+
+const handleKeyDown = (e) => {
+    if (!hintButtonClicked) {
+        hintButton.disabled = false;
+    }
+
+    giveHint(arrayOfCharacters, e.key);
+
+    if (arrayOfCharacters.includes(e.key)) {
+        const allLineValues = document.querySelectorAll(".line-value");
+
+        for (let i = 0; i < arrayOfCharacters.length; i++) {
+            if (arrayOfCharacters[i] === e.key) {
+                checkCorrectAnswer(arrayOfCharacters, e.key);
+
+                allLineValues[i].classList.add("show");
+            }
+        }
+    } else {
+        setWrongCharacters(arrayOfWrongCharacters, e);
+        checkWrongAnswers(arrayOfWrongCharacters);
+    }
+};
+
+function handleHintButtonClick() {
+    hintButton.disabled = true;
+    hintButtonClicked = true;
+
+    const randomIdx = Math.floor(Math.random() * arrayOfPossibleHints.length);
+
+    const randomCharacter = arrayOfPossibleHints[randomIdx];
+
+    silverBox({
+        alertIcon: "info",
+        title: `This word contains this character: "${randomCharacter}".`,
+        centerContent: true,
+    });
+}
+
+const giveHint = (characters, pressedKey) => {
+    try {
+        if (!arrayOfGuessedCharacters.includes(pressedKey)) {
+            arrayOfGuessedCharacters.push(pressedKey);
+        }
+
+        arrayOfPossibleHints = characters.filter(
+            (char) => !arrayOfGuessedCharacters.includes(char)
+        );
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default giveHint;
